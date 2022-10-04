4d72b36f63a4a40a163918c70765ac15d4a7217e
hyogeun-p <wdae1204@gmail.com>
2022-10-04 12:47:45 +0900

    박효근 커밋

--- team4-final-project/src/main/java/com/goott/mapper/QnAMapper.java
+++ team4-final-project/src/main/java/com/goott/mapper/QnAMapper.java
@@ -12,20 +12,37 @@ import com.goott.domain.QnAVO;
 public interface QnAMapper {
 //	public List<QnAVO> list();
 	
+	// 게시물 작성
 	public void Question(QnAVO QnA);
-
+	
+	// 게시물 상세 페이지
 	public Object QnA_detail(Map<String, Object> map);
 
+	
+	// 게시물 수정
 	public void QnA_update(QnAVO qnA);
-
+	
+	// 게시물 삭제
 	public void delete(QnAVO QnA);
 	// 게시물 총 갯수
 	public int countQnABoard();
 
 	// 페이징 처리 게시글 조회
 	public List<QnAVO> selectQnABoard(Map<String, Object> map);
+	
+	// 카테고리 및 검색으로 게시글 총 개수 조
+	public int countQnABoardCategory(Map<String, Object> map);
 
-	public int countQnABoardCategory(String ckeckcategory);
+	public void Question_admin(QnAVO qnA);
+
+	public void Question_admin_answer(QnAVO qnA);
+
+	
+
+
+
+
+
 
 }
 
